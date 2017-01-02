543b5f245d0961cb32c4a1306094fb8e1dd31554
Adrian Bolinger <adrian.bolinger@me.com>
2017-01-02 06:48:02 -0500

    Initial Commit

--- EnableTextFieldDemo/ViewController.swift
+++ EnableTextFieldDemo/ViewController.swift
@@ -9,17 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    
+    // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // this is the initial setting, as your textfield is presumambly blank
+        configureTextFields()
+        configureDismissKeyboard()
+        updateTextField()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Configuration Methods
+    func configureTextFields() {
+        // create an array of textfields
+        let textFieldArray = [firstNameTextField, lastNameTextField, phoneNumberTextField]
+        
+        // configure them...
+        for textField in textFieldArray {
+            // make sure you set the delegate to be self
+            textField?.delegate = self
+            // add a target to them
+            textField?.addTarget(self, action: #selector(ViewController.updateTextField), for: .editingChanged)
+        }
     }
+    
+    func configureDismissKeyboard() {
+        // creates a gesture recognizer that calls dismissKeyboard and adds to the view
+        let tapToDismissKeyboard = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tapToDismissKeyboard)
+    }
+    
+    // this is the target that gets called when editing changes
+    func updateTextField() {
+        // create an array of textFields
+        let textFields = [firstNameTextField, lastNameTextField, phoneNumberTextField]
+        // create a bool to test if a textField is blank in the textFields array
+        let oneOfTheTextFieldsIsBlank = textFields.contains(where: {($0?.text ?? "").isEmpty})
+        if oneOfTheTextFieldsIsBlank {
+            continueButton.isEnabled = false
+            continueButton.alpha = 0.5
+            print("** One of the textFields is blank **\ncontinueButton.isEnabled == false")
+        } else {
+            continueButton.isEnabled = true
+            continueButton.alpha = 1.0
+            print("** continueButton.isEnabled == true")
+        }
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    // MARK: - Button Actions
+    
+    @IBAction func continueButtonAction(_ sender: UIButton) {
+        print("continueButton can do something")
+    }
+    
+}
 
-
+// MARK: - UITextFieldDelegate Methods
+// you can also put UITextFieldDelegate after UIViewController and tuck the methods inside that class. I like to keep delegate methods in extensions for readability
+extension ViewController: UITextFieldDelegate {
+    // you can fill this in if you need any delegate methods.
 }
 
